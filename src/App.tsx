1e8b2ba1e33595ee00f68e6e826e0c807f383dbc
Matias Martinez <tinezmatias@gmail.com>
2022-01-26 08:44:55 -0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react'
-import logo from './logo.svg'
+// Vendor
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// Internal
+import Discover from './containers/Discover'
+import Details from './containers/Details'
+// Styles
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Discover />} />
+        <Route path="/movie/:id" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
